2a775912ff7de2460fb5c71334b01a314e98ef4b
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2024-01-07 21:46:46 +0100

    rajout code supprimer par erreur

--- app/src/main/java/com/example/popinteraction/emojistory/EmojiStoryFrontPage.kt
+++ app/src/main/java/com/example/popinteraction/emojistory/EmojiStoryFrontPage.kt
@@ -3,8 +3,10 @@ package com.example.popinteraction.emojistory
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.popinteraction.DepixelimageActivity
+import com.example.popinteraction.MainActivity
 import com.example.popinteraction.R
 
 class EmojiStoryFrontPage: AppCompatActivity() {
@@ -17,6 +19,23 @@ class EmojiStoryFrontPage: AppCompatActivity() {
     fun play(view: View){
         val intent = Intent(this, EmojiStoryActivity::class.java)
         startActivity(intent)
+    }
+
+    fun navigateToMenu(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun showRulesPopup(view: View) {
+        val alertDialogBuilder = AlertDialog.Builder(this, R.style.CustomDialog)
+        alertDialogBuilder.setTitle(R.string.rules_popup_title)
+        alertDialogBuilder.setMessage(R.string.rules_emoji_story)
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
 }
